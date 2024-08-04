8f0580c77d3235e853f373e016d6b7287c411742
Ekwutozia Onyebuchi Maureen <165788844+ReinePearlBuchi@users.noreply.github.com>
2024-08-04 09:03:43 +0100

    update

---
+++ main.py
@@ -0,0 +1,26 @@
+
+# user_text = "Enter a todo: "
+
+# todo1 = input(user_text)
+# todo2 = input(user_text)
+# todo3 = input(user_text)
+#
+# todos = [todo1, todo2, todo3]
+# print(todos)
+#
+# print(type(todos))
+#
+# print(len(todos))
+
+# todos = []
+# while 2 > 1:
+#     todo = input(user_text)
+#     todos.append()
+#     todos.append(todo)
+#     print(todos)
+
+password = input("Enter your password: ")
+
+while password != "pass123":
+    password = input("Enter your password: ")
+print(password)
---
+++ random_excercise/randomisation.py
@@ -0,0 +1,17 @@
+# import random
+# #
+# # love_score = random.randint(1, 100)
+# # print(f"Your love score is {love_score}")
+#
+# random_side = random.randint(0,1)
+# if random_side == 1:
+#     print("Heads")
+# else:
+#     print("Tails")
+#
+
+state_of_america = ["Alabama", "Alaska", "America"]
+print(state_of_america)
+
+state_of_america.append(["buchiland", "California"])
+print(state_of_america)
